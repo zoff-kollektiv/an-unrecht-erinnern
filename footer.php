903581cc91f581f6b1d063ff1d1d354d4a518a34
Gustav Pursche <pursche@posteo.de>
2019-08-28 12:12:53 +0200

    Biography: add hero header styles

--- footer.php
+++ footer.php
@@ -1,11 +1,7 @@
 <footer class="footer">
-  <?php
-    wp_nav_menu(
-      array(
-        'theme_location' => 'footer'
-      )
-    );
-  ?>
+  <?php wp_nav_menu(array(
+      'theme_location' => 'footer'
+  )); ?>
 </footer>
 
 <?php wp_footer(); ?>
--- index.php
+++ index.php
@@ -1,15 +1,32 @@
 <?php get_header(); ?>
 
 <main class="main">
+  <?php if (have_posts()) :
+      while (have_posts()) :
+          the_post();
+  ?>
+
+  <header class="hero">
+    <?php the_post_thumbnail('header', [
+      'class' => 'hero__image'
+    ]); ?>
+
+    <div class="hero__content">
+      <h1 class="hero__title">
+        <?php the_title(); ?>
+      </h1>
+
+      <div class="hero__excerpt">
+        <?php the_excerpt(); ?>
+      </div>
+    </div>
+  </header>
+
   <div class="block-content">
     <?php
-      if (have_posts()) {
-        while (have_posts()) {
-          the_post();
-          the_title();
           the_content();
-        }
-      }
+        endwhile;
+      endif;
     ?>
   </div>
 </main>
