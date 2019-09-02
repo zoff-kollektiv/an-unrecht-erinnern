a1971f158f0baeeba9dc117ac70c7fffed792451
Gustav Pursche <pursche@posteo.de>
2019-09-02 16:05:45 +0200

    Video: improve video styling

--- blocks/video.php
+++ blocks/video.php
@@ -8,21 +8,30 @@ $image_id = get_field('image');
 ?>
 
 <a href="<?php echo $url; ?>" class="video">
-  <?php echo wp_get_attachment_image($image_id, 'video', false, [
-      'class' => 'video__image',
-      'loading' => 'lazy'
-  ]); ?>
+  <div class="video__inner-container">
+    <?php echo wp_get_attachment_image($image_id, 'video', false, [
+        'class' => 'video__image',
+        'loading' => 'lazy'
+    ]); ?>
 
-  <div class="video__content">
-    <span class="video__length">
-      <?php echo $length; ?>
-    </span>
+    <div class="video__content">
+      <div class="video__play-container">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 90 70" class="video__play">
+          <rect width="105" height="70" x="-5px" y="0" fill="#B11414" />
+          <path fill="#fff" d="M36.8257 48.8762V22.1238L58.795 35.6783 36.8257 48.8762z"/>
+        </svg>
 
-    <h3 class="video__title">
-      <?php echo $title; ?>
-    </h3>
-    <p class="video__description">
-      <?php echo $description; ?>
-    </p>
+        <span class="video__length">
+          <?php echo $length; ?>
+        </span>
+      </div>
+
+      <h3 class="video__title">
+        <?php echo $title; ?>
+      </h3>
+      <p class="video__description">
+        <?php echo $description; ?>
+      </p>
+    </div>
   </div>
 </a>
