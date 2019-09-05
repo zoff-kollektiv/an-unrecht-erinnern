73a20994b0198edc3f20a1b95be7c5c44a69cfe2
Gustav Pursche <pursche@posteo.de>
2019-09-05 12:15:55 +0200

    Vita: refactor block

--- blocks/vita.php
+++ blocks/vita.php
@@ -1,18 +1,22 @@
-<ul class="vita">
-<?php if (have_rows('entries')):
-    while (have_rows('entries')):
-        the_row(); ?>
+<div class="wp-block-vita">
+  <ul class="wp-block-vita__list">
+  <?php if (have_rows('entries')):
+      while (have_rows('entries')):
+          the_row(); ?>
 
-<li class="vita__item">
-  <div class="vita__item-container">
-    <p class="vita__item-description">
-      <strong class="vita__item-year"><?php the_sub_field('year'); ?></strong>
-      <?php the_sub_field('description'); ?>
-    </p>
-  </div>
-</li>
+  <li class="wp-block-vita__item">
+    <div class="wp-block-vita__item-container">
+      <p class="wp-block-vita__item-description">
+        <strong class="wp-block-vita__item-year"><?php the_sub_field(
+            'year'
+        ); ?></strong>
+        <?php the_sub_field('description'); ?>
+      </p>
+    </div>
+  </li>
 
-<?php
-    endwhile;
-endif; ?>
-</ul>
+  <?php
+      endwhile;
+  endif; ?>
+  </ul>
+</div>
