6dceab1934d930d88aad4da5ca13201974f9e65d
Gustav Pursche <pursche@posteo.de>
2019-11-11 21:56:11 +0100

    Replae subgrid with grid mixin 😢

--- components/menu/languages.php
+++ components/menu/languages.php
@@ -1,12 +1,11 @@
 <?php
-  $languages = pll_the_languages([
+$languages = pll_the_languages([
     'hide_current' => true,
     'hide_empty' => false,
     'raw' => true
-  ]);
-?>
+]); ?>
 
-<?php foreach($languages as $language) : ?>
+<?php foreach ($languages as $language): ?>
   <a href="<?php echo $language['url']; ?>" class="language">
     <?php echo $language['name']; ?>
   </a>
