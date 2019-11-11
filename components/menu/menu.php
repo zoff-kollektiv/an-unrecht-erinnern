53b914c54b153adf73413dd43624a0d17d4cb414
Gustav Pursche <pursche@posteo.de>
2019-11-11 14:00:09 +0100

    Header: fix menu in Safari

--- components/menu/menu.php
+++ components/menu/menu.php
@@ -1,15 +1,17 @@
 <details class="<?php echo $location; ?>-menu">
   <summary class="<?php echo $location; ?>-menu__toggle">
-    <svg aria-hidden="true" focusable="false" role="img" class="<?php echo $location; ?>-menu__bars" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-      <path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path>
-    </svg>
+    <div>
+      <svg aria-hidden="true" focusable="false" role="img" class="<?php echo $location; ?>-menu__bars" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+        <path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path>
+      </svg>
 
-    <svg aria-hidden="true" focusable="false" role="img" class="<?php echo $location; ?>-menu__close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 18">
-      <path fill="#828282" d="M14.8491.1875l2.1213 2.1213L2.1212 17.1581l-2.1213-2.1214z" />
-      <path fill="#828282" d="M.0002 2.3088L2.1215.1875l14.8492 14.8493-2.1213 2.1213z" />
-    </svg>
+      <svg aria-hidden="true" focusable="false" role="img" class="<?php echo $location; ?>-menu__close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 18">
+        <path fill="#828282" d="M14.8491.1875l2.1213 2.1213L2.1212 17.1581l-2.1213-2.1214z" />
+        <path fill="#828282" d="M.0002 2.3088L2.1215.1875l14.8492 14.8493-2.1213 2.1213z" />
+      </svg>
 
-    <span class="<?php echo $location; ?>-menu__toggle-label">Menü</span>
+      <span class="<?php echo $location; ?>-menu__toggle-label">Menü</span>
+    </div>
   </summary>
 
   <ul class="<?php echo $location; ?>-menu__inner">
